1d7e1372e74fd6d7e2f06531d3d1b4b8141de0ce
Susan Harkey <susanharkey@gmail.com>
2014-10-22 17:29:26 -0500

    Finished

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,17 @@
-// Add your code here
+
+
+$('.filter-button').on('click', function () {
+  $('.' + this.id).toggle();
+  $(this).toggleClass('active');
+  alertify.log("Toggled"+" "+this.id);
+
+  // if($(this).hasClass('active')) {
+  //   console.log('has class!');
+  //   $(this).removeClass('active');
+  // } else {
+  //   console.log('does not has class!');
+  //   $(this).addClass('active');
+  // }
+
+});
+
